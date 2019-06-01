ed27c9cb7891b1d7ca3bf1b3c929506626a4b127
antonmolchanov90 <amol4anoff@mail.ru>
2019-06-01 13:41:50 +0600

    new commit

--- UI_Tests/Python_Selenium/FacebookLogin_UI_Test.py
+++ UI_Tests/Python_Selenium/FacebookLogin_UI_Test.py
@@ -4,8 +4,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from LoginDetails import*
 
-#NEGATIVE TEST
+
+
+#POSITIVE CASE - SIMPLE VERIFICATION
+
+#Open browser, enter Facebook
+driver = webdriver.Chrome("/opt/chromedriver")
+driver.get("https://www.facebook.com")
+
+#Locate elements, pass email & password (both empty), click Log In button
+driver.find_element_by_id("email").send_keys(login)
+driver.find_element_by_id("pass").send_keys(password)
+driver.find_element_by_id("loginbutton").click()
+
+#Assert successfull login
+if driver.title == 'Facebook':
+ print('Passed')
+else:
+ print('Error')
+
+#Close browser
+driver.quit()
+
+
+
+#NEGATIVE CASE 1
 
 #Open browser, enter Facebook
 driver = webdriver.Chrome("/opt/chromedriver")
@@ -16,7 +41,30 @@ driver.find_element_by_id("email").send_keys("")
 driver.find_element_by_id("pass").send_keys("")
 driver.find_element_by_id("loginbutton").click()
 
-#Assert successfull login
+#Assert unsuccessfull login
+if driver.title == 'Log into Facebook | Facebook':
+ print('Passed')
+else:
+ print('Error')
+
+#Close browser
+driver.quit()
+
+
+
+
+#NEGATIVE CASE 2
+
+#Open browser, enter Facebook
+driver = webdriver.Chrome("/opt/chromedriver")
+driver.get("https://www.facebook.com")
+
+#Locate elements, pass email & password (both empty), click Log In button
+driver.find_element_by_id("email").send_keys("amolchanoff@mail.ru")
+driver.find_element_by_id("pass").send_keys("")
+driver.find_element_by_id("loginbutton").click()
+
+#Assert unsuccessfull login
 if driver.title == 'Log into Facebook | Facebook':
  print('Passed')
 else:
---
+++ UI_Tests/Python_Selenium/LoginDetails.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#Facebook Password:
+login = 'amol4anoff@mail.ru'
+password = 'julien020790!'
