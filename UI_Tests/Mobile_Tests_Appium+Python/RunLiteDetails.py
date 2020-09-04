412ebfff04a3a52fdbb35029500d043caea42f52
antonmolchanov90 <amol4anoff@mail.ru>
2020-09-04 20:34:05 +0600

    separation

---
+++ UI_Tests/Mobile_Tests_Appium+Python/RunLiteDetails.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+user = "qaqc"
+pwrd = "test123"
+sum_amount = "100"
+test_quantity = "1"
+cost = "2"
+cashier_num = "100"
+company = "TestCompany"
+phone_num = "+996777111111"
+store_name = "test-shop1"
+email_addr = "test@mail.com"
